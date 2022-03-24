92a9bcfb5492fa761de060d2a4f8e0450b30f9a9
Blastmerder <100530405+Blastmerder@users.noreply.github.com>
2022-03-24 18:17:21 +0300

    gef

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from WeatherForecast import WeatherForecast
 import schedule
 from threading import Thread
 
-time_seten = "07:00"
+time_set = "07:00"
 
 last_command = ""
 
 bot = telebot.TeleBot("5017889526:AAHU6ExmufVnabKqA5UBZPlIKmvr2IPtJjw")
-weather_forcast = WeatherForecast()
+weather_forecast = WeatherForecast()
 
 bot.set_my_commands([
     types.BotCommand("set_city", "Вам дадут инструкции"),
     types.BotCommand("set_lang", "Вам дадут инструкции"),
     types.BotCommand("set_units", "Вам дадут инструкции"),
-    types.BotCommand("set_time_masage", "Вам дадут инструкции"),
+    types.BotCommand("set_time_mesage", "Вам дадут инструкции"),
     types.BotCommand("schedule", "Вам дадут инструкции")
 ])
 
@@ -32,7 +32,7 @@ def schedulee(message):
 
 def send_hello():
     global id
-    bot.send_message(id, f'Сейчас в городе {weather_forcast.get_data().json()["name"]} {weather_forcast.get_data().json()["weather"][0]["description"]}.')
+    bot.send_message(id, f'Сейчас в городе {weather_forecast.get_data().json()["name"]} {weather_forecast.get_data().json()["weather"][0]["description"]}.')
 
 
 @bot.message_handler(commands=['start'])
@@ -61,13 +61,13 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == "metric")
 def callback(call):
-    weather_forcast.set_units("metric")
+    weather_forecast.set_units("metric")
     bot.send_message(call.message.chat.id, "успешно установлена метрическая система.")
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "imperic")
 def callback(call):
-    weather_forcast.set_units("imperic")
+    weather_forecast.set_units("imperic")
     bot.send_message(call.message.chat.id, "успешно установлена импееская система.")
 
 
@@ -80,28 +80,28 @@ def set_lang(message):
 
 @bot.message_handler(commands=['weather'])
 def get_weather(message):
-    bot.send_message(message.chat.id, f'Сейчас в городе {weather_forcast.get_data().json()["name"]} {weather_forcast.get_data().json()["weather"][0]["description"]}.')
+    bot.send_message(message.chat.id, f'Сейчас в городе {weather_forecast.get_data().json()["name"]} {weather_forecast.get_data().json()["weather"][0]["description"]}.')
 
 
 @bot.message_handler(content_types=["text"])
 def set_city(message):
-    global time_seten
+    global time_set
     global last_command
     global job
     if last_command == "start":
-        if weather_forcast.set_city(message.text):
+        if weather_forecast.set_city(message.text):
             bot.send_message(message.chat.id, f"успешно установлен город {message.text}")
             last_command = ""
         else:
             bot.send_message(message.chat.id, "Введите свой город ещё раз")
     elif last_command == "set_lang":
-        if weather_forcast.set_lang(message.text):
+        if weather_forecast.set_lang(message.text):
             bot.send_message(message.chat.id, f"успешно установлен язык {message.text}")
             last_command = ""
         else:
             bot.send_message(message.chat.id, "Введите свой язык ещё раз")
     elif last_command == "set_units":
-        if weather_forcast.set_lang(message.text):
+        if weather_forecast.set_lang(message.text):
             bot.send_message(message.chat.id, f"успешно установлена единица измерения {message.text}")
             last_command = ""
         else:
@@ -123,13 +123,12 @@ def set_city(message):
             bot.send_message(message.chat.id, "Введите свои единицы измерения ещё раз")
 
 
-
 def schedule_checker():
     while True:
         schedule.run_pending()
 
 
-job = schedule.every().day.at(time_seten).do(send_hello)
+job = schedule.every().day.at(time_set).do(send_hello)
 Thread(target=schedule_checker).start()
 
 bot.polling(none_stop=True, interval=0)
